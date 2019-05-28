88e69fb8b2de340136ac9ffc12bb23e6560beec3
Clint Kunz <clintkunz@gmail.com>
2019-05-28 16:11:05 -0600

    fizzbuzz complete, all tests pass

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,22 @@
 */
 int fizzbuzz(int n)
 {
-
+    int counter = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0) {
+            if (i % 5 == 0) {
+                printf("FizzBuzz\n");
+            } else {
+                printf("Fizz\n");
+            }
+        } else if (i % 5 == 0) {
+            printf("Buzz\n");
+        } else {
+            counter++;
+            printf("%d\n", i);
+        }
+    }
+    return counter;
 }
 
 #ifndef TESTING
